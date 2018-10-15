6fb009d2be2fc0a9474b523e3bff9633904a814b
joaobalaia <44161602+joaobalaia@users.noreply.github.com>
2018-10-15 12:08:41 +0100

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,17 +1,44 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp1
+namespace ConsoleApplication4
 {
     class Program
     {
         static void Main(string[] args)
         {
-            //Coloque aqui o seu codigo
-        
+            double x;
+            double y;
+
+            Console.WriteLine("digite um valor para o x");
+            x = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("digite um valor para o y");
+            y = Convert.ToDouble(Console.ReadLine());
+
+            if (x > 0 && y > 0)
+                Console.WriteLine("esta no 1 quadrante");
+            else
+            
+                if (x > 0 && y < 0)
+                    Console.WriteLine("esta no 4 quadrante");
+            
+            else
+            
+                if (x<0 && y >0)
+                    Console.WriteLine("esta no 2 quadrante");
+            
+            else 
+            Console.WriteLine("esta no 3 quadrante");
+
+
+
+
+
+
         }
     }
 }
